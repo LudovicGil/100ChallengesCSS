d88d95f226c894c6f327476f3282765cfd66f460
Ludovic Gil <contact@ludovicgil.fr>
2020-06-21 14:32:22 +0200

    ajout de fonctionnalités au chargement de la page

--- Day 1/index.js
+++ Day 1/index.js
@@ -1,5 +1,14 @@
-let menuIcons = document.querySelector(".menuIcons");
+const menuIcons = document.querySelector(".menuIcons");
+const line = document.querySelectorAll('.no-active');
 
-menuIcons.addEventListener('click', function() {
-        this.classList.toggle('active');
+/*
+Ecoute sur le bouton pour ajouter la classe "active"
+et retirer la classe "no-active" qui empeche l'activation
+de l'animation au chargement de la page
+*/
+menuIcons.addEventListener('click', function () {
+    this.classList.toggle('active');
+    for (const element of line) {
+        element.classList.remove('no-active');
+    }
 });
